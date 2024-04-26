9c550a83de2f8c846c0cf70b1985b760584979ae
laxyshkaaa <parvadov.pavel@gmail.com>
2024-04-26 20:37:24 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,26 @@
+import java.io.*;
+import java.util.regex.*;
+
+public class Main {
+    public static void main(String[] args) {
+        try (BufferedReader reader = new BufferedReader(new FileReader("C:\\Users\\Pavel\\IdeaProjects\\TegsDOp\\INPUT.txt"));
+             BufferedWriter writer = new BufferedWriter(new FileWriter("C:\\Users\\Pavel\\IdeaProjects\\TegsDOp\\OUTPUT.txt"))) {
+
+            String line;
+            StringBuilder sb = new StringBuilder();
+            while ((line = reader.readLine()) != null) {
+                sb.append(line);
+                sb.append("\n");
+            }
+            String content = sb.toString();
+
+            Pattern emptyTagPattern = Pattern.compile("<([a-zA-Z]+)>\\s*</\\1>");
+            Matcher matcher = emptyTagPattern.matcher(content);
+            String cleanedContent = matcher.replaceAll("");
+
+            writer.write(cleanedContent);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
